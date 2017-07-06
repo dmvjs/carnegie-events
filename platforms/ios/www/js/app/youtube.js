50acf46e96e781d5fe21fea57a6bcc10282afe24
Kirk Elliott <kirk@dmvjs.com>
2017-07-05 21:44:13 -0400

    og youtube iframe setup

--- platforms/ios/www/js/app/youtube.js
+++ platforms/ios/www/js/app/youtube.js
@@ -1,9 +1,9 @@
+"use strict";
+
 // 2. This code loads the IFrame Player API code asynchronously.
 var player;
 var playerReference;
 var tag;
-var frameset;
-var frame;
 
 if (tag !== undefined) {
     document.head.removeChild(tag);
@@ -13,17 +13,10 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-
 function createPlayer () {
-    frameset = document.createElement("frameset");
-    frameset.id = "frameset";
-    frame = document.createElement("frame");
-    frame.id = "frame";
     player = document.createElement('div');
     player.id = "player";
-    frame.append(player);
-    frameset.append(frame);
-    $(document.body).append(frameset);
+    $(document.body).append(player);
     onYouTubeIframeAPIReady();
     setTimeout( function (){
         var p = $('#player');
@@ -37,7 +30,6 @@ function removePlayer () {
         playerReference.destroy();
         $('.show-tv').removeClass('active');
         $('#player').remove();
-        $('#frameset').remove();
     }, 500);
 }
 
@@ -59,6 +51,4 @@ function onYouTubeIframeAPIReady() {
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     playerReference = event.target;
-    var ref = document.getElementById('player');
-    frame.src = ref.src;
 }
