c8774587ebdcc34a2cfc1c376c8e1d7295b109c6
Kirk Elliott <cappinkirk@gmail.com>
2017-09-30 08:43:46 -0400

    fixed untracked files

--- platforms/android/assets/www/js/io/getDir.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (filesystem, dirname) {
-	return new Promise(function (resolve, reject) {
-		var fileentry = filesystem.root;
-		fileentry.getDirectory(dirname || config.folder, {create: false, exclusive: false}, resolve, reject);
-	});
-}
