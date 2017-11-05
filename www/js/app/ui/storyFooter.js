67b7685c7393dc37e3a7e47ac3feaff46952768f
Kirk Elliott <cappinkirk@gmail.com>
2017-11-05 14:02:59 -0500

    adds twitter and survey JS

--- www/js/app/ui/storyFooter.js
+++ www/js/app/ui/storyFooter.js
@@ -1,6 +1,7 @@
 var story = require('./story');
 
 $('footer.story-footer .twitter').on('click', function () {
+    $('#survey-container').removeClass('active');
     $('#twitter-container').empty();
     var data = story.getCurrentPageData();
     if (data !== undefined && data.twitterID !== undefined) {
@@ -11,25 +12,48 @@ $('footer.story-footer .twitter').on('click', function () {
             },
             document.getElementById('twitter-container')
         );
-        $('section.twitter').toggleClass('active')
+        $('section.twitter').toggleClass('active');
+        var closeButton = $('<div/>', {
+            addClass: 'close-iframe',
+            text: '✕'
+        }).on('click', function () {
+            closeTwitterAndSurvey();
+        });
+        $('#twitter-container').prepend(closeButton);
     }
 });
 
-function showTwitterInFooter () {
-    $(data.twitterID).show()
-}
-
-function showTwitterInFooterWithLogic () {
+$('footer.story-footer .survey').on('click', function () {
+    $('#twitter-container').removeClass('active');
+    var sc = $('#survey-container');
+    sc.empty();
     var data = story.getCurrentPageData();
-    $(data.twitterID).toggle((data !== undefined && data.twitterID !== undefined))
+    if (!!data.survey) {
+        var iframe = document.createElement('iframe');
+        iframe.scrolling = 'no';
+        iframe.width = '100%';
+        iframe.height = '' + $(document).height() - (88 + 44 + 20 + 44) + 'px';
+        iframe.src = data.survey;
+        sc.eq(0).append(iframe);
+        $('section.survey').toggleClass('active');
+        var closeButton = $('<div/>', {
+            addClass: 'close-iframe',
+            text: '✕'
+        }).on('click', function () {
+            closeTwitterAndSurvey();
+        }).hide();
+        setTimeout(function () {
+            $('#survey-container').prepend(closeButton);
+            closeButton.fadeIn();
+        }, 200);
+    }
+});
+
+function closeTwitterAndSurvey () {
+    $('section.survey').removeClass('active');
+    $('section.twitter').removeClass('active');
 }
 
-function hideTwitterInFooter () {
-    $(data.twitterID).hide();
-}
+$('footer.story-footer .close-iframe').on('click', function () {
 
-module.exports = {
-    showTwitterInFooter: showTwitterInFooter,
-    hideTwitterInFooter: hideTwitterInFooter,
-    showTwitterInFooterWithLogic: showTwitterInFooterWithLogic
-};
+});
